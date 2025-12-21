26106ed42dae892df37f2b01de879380e9181c7e
Mazen Mohamed Shaban <mazen.mohamed@viavisolutions.com>
2025-12-21 22:57:36 +0200

    feat: initialize SolidJS application with routing and styling
    - Add pnpm workspace configuration for built dependencies.
    - Include favicon for the application.
    - Create global CSS file with TailwindCSS integration and dark mode support.
    - Implement main application component with routing and navigation.
    - Add a Counter component for demonstration purposes.
    - Create a navigation component with links to Home, About, and Particles pages.
    - Set up client and server entry points for SolidJS.
    - Define global TypeScript types for SolidJS environment.
    - Implement 404 Not Found page with navigation links.
    - Create About page with Counter component.
    - Create Home page with Counter component.
    - Configure TypeScript with strict settings and module resolution.

---
+++ src/components/Nav.tsx
@@ -0,0 +1,39 @@
+import { useLocation } from "@solidjs/router";
+import { For } from "solid-js";
+
+export default function Nav() {
+  const location = useLocation();
+  const active = (path: string) =>
+    path === location.pathname
+      ? "border-sky-600"
+      : "border-transparent hover:border-sky-600";
+
+  const navItems = [
+    {
+      href: "/",
+      title: "Home",
+    },
+    {
+      href: "/about",
+      title: "About",
+    },
+    {
+      href: "/particles",
+      title: "Particles",
+    },
+  ];
+
+  return (
+    <nav class="bg-sky-800">
+      <ul class="container flex items-center p-3 text-gray-200 gap-1.5">
+        <For each={navItems}>
+          {(item) => (
+            <li class={`border-b-2 ${active(item.href)} mx-1.5 sm:mx-6`}>
+              <a href={item.href}>{item.title}</a>
+            </li>
+          )}
+        </For>
+      </ul>
+    </nav>
+  );
+}
